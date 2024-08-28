7f8c97452dd522b48b22a1a11674be92919158a3
QbinVA <valdokevin02@gmail.com>
2024-08-28 07:42:30 -0600

    fondo en movimiento

--- JuegoInsano.py
+++ JuegoInsano.py
@@ -4,6 +4,7 @@ import sys
 #Importo la pantalla
 import pygame.display
 from pygame.locals import *
+from button import Button
 
 #Inicializo pygame
 pygame.init()
@@ -18,30 +19,21 @@ hc74225 = (199,66,37)
 h61cd35 = (97,205,53)
 
 #Declaro la variable pantalla con sus respectivos valores
-pantalla = pygame.display.set_mode((500, 750))
+W,H = 500, 750
+pantalla = pygame.display.set_mode((W, H))
+fps = 90
+reloj = pygame.time.Clock()
+
 #Inserto un caption
-pygame.display.set_caption('Juego insano')
+pygame.display.set_caption('Hungry Jump')
 
-#Inserto color en la pantalla
-pantalla.fill(blanco)
+#Declaro e inserto el icono
+icono = pygame.image.load("images/banana.png")
+pygame.display.set_icon(icono)
 
-#Declaro un rectangulo (donde, color, posicion, tamaño)
-rect1 = pygame.draw.rect(pantalla, rojo, (100, 50, 100, 50))
-
-#Declaro una linea (donde, color, (inicio y fin, inclinacion), grosor)
-line1 = pygame.draw.line(pantalla, verde, (100, 104), (199,104), 10)
-
-#Deckaro un circulo (donde, color, (eje x, eje y), radio, relleno)
-circle1 = pygame.draw.circle(pantalla, negro, (122, 250), 20, 0)
-
-#Declaro un elipse (donde, color, (eje x, eje y, tamaño x, tamaño y), relleno)
-elipse1 = pygame.draw.ellipse(pantalla, h61cd35, (275, 200, 40, 80), 10)
-
-#declaro puntos
-puntos = [(100,300),(100,100),(150,100)]
-#los uso para crear una figura
-pygame.draw.polygon(pantalla, (0,150, 255), puntos, 8)
-
+#Declaro e inserto el fondo
+fondo = pygame.image.load("images/fondok.jpg").convert()
+x = 0
 
 #Ciclo infinito
 while True:
@@ -49,4 +41,10 @@ while True:
         if event.type == quit:
             pygame.quit()
             sys.exit()
+    xRelativa = x % fondo.get_rect().width
+    pantalla.blit(fondo, (xRelativa - fondo.get_rect().width, 0))
+    if xRelativa < W:
+        pantalla.blit(fondo, (xRelativa, 0))
+    x -= 1
     pygame.display.update() #Se actualiza la pantalla
+    reloj.tick(fps)
