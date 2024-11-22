ef449e58e02fe39d37dea3258300dc862ea07a17
QbinVA <valdokevin02@gmail.com>
2024-11-21 21:01:04 -0600

    config selected

--- starter.py
+++ starter.py
@@ -57,6 +57,8 @@ def main_menu():
     tuerca = pygame.image.load("assets/images/menu/Config.png")
     tuerca = pygame.transform.scale(tuerca, (110, 110))
 
+    tuercaSelected = pygame.image.load("assets/images/menu/Config selected.png")
+
     bPlay = pygame.image.load("assets/images/menu/StartButton.png")
     bPlay = pygame.transform.scale(bPlay, (300, 180))
 
@@ -92,8 +94,14 @@ def main_menu():
             PLAY_BUTTON = Button(image=bPlay, pos=(250, 530), 
                                 text_input="", font=get_font(20), base_color="#d7fcd4", hovering_color="White")
 
-        OPTIONS_BUTTON = Button(image=tuerca, pos=(435, 65), 
-                            text_input="", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
+        # Cambiar imagen del botón de opciones según hover
+        if 400 < MENU_MOUSE_POS[0] < 470 and 30 < MENU_MOUSE_POS[1] < 100:  # Ajustar las coordenadas según el botón
+            OPTIONS_BUTTON = Button(image=pygame.transform.scale(tuercaSelected, (130, 130)), pos=(430, 75), 
+                                    text_input="", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
+        else:
+            OPTIONS_BUTTON = Button(image=tuerca, pos=(430, 70), 
+                                    text_input="", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
+            
         QUIT_BUTTON = Button(image=pygame.image.load("assets/images/menu/botonSalir.png"), pos=(70, 90), 
                             text_input="", font=get_font(22), base_color="#d7fcd4", hovering_color="White")
 
